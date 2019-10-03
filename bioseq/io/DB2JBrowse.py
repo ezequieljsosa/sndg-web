7323699304adcb09e040311b0a585631201567b8
Ezequiel Sosa <ezequieljsosa@gmail.com>
2019-10-03 19:17:45 -0300

    absolute jbrowse_data_path

--- bioseq/io/DB2JBrowse.py
+++ bioseq/io/DB2JBrowse.py
@@ -144,14 +144,14 @@ class DB2JBrowse():
         http://jbrowse.org/docs/html_features.html
         """
         sys.stderr.write("prepare-refseqs\n")
-        cmd = """docker run -v {jbrowse_path}/{jbrowse_data_path}:/jbrowse/data/ -v {fasta_path}:/tmp/jbrowse.fasta \
+        cmd = """docker run -v {jbrowse_data_path}:/jbrowse/data/ -v {fasta_path}:/tmp/jbrowse.fasta \
         jbrowse/jbrowse-1.12.0 bin/prepare-refseqs.pl --fasta /tmp/jbrowse.fasta --out data/{acc} --key "Sequence"
         """.format(jbrowse_path=self.jbrowse_path, jbrowse_data_path=self.jbrowse_data_path,
                    acc=self.accession, fasta_path=self.fasta_path)
         sp.call(cmd, shell=True)
 
         sys.stderr.write("flatfile-to-json.pl\n")
-        cmd = """docker run -v {jbrowse_path}/{jbrowse_data_path}:/jbrowse/data/ -v {gff_path}:/tmp/jbrowse.gff \
+        cmd = """docker run -v {jbrowse_data_path}:/jbrowse/data/ -v {gff_path}:/tmp/jbrowse.gff \
          jbrowse/jbrowse-1.12.0 ./bin/flatfile-to-json.pl --gff "/tmp/jbrowse.gff" --out "/jbrowse/data/{acc}" \
           --key "{label}" --trackLabel "{label}" --trackType CanvasFeatures --nameAttributes "gene,locus_tag" \
            --clientConfig '{clientConfig}' 
@@ -167,7 +167,7 @@ class DB2JBrowse():
         sp.call(cmd, shell=True)
 
         sys.stderr.write("generate-names\n")
-        cmd = """docker run -v {jbrowse_path}/{jbrowse_data_path}:/jbrowse/data/  \
+        cmd = """docker run -v {jbrowse_data_path}:/jbrowse/data/  \
          jbrowse/jbrowse-1.12.0 ./bin/generate-names.pl  --out "/jbrowse/data/{acc}" --tracks "{labels}" 
         """.format(jbrowse_path=self.jbrowse_path, jbrowse_data_path=self.jbrowse_data_path,
                    acc=self.accession, labels=",".join(["Sequence", "Genes"]))  # --className  feature
